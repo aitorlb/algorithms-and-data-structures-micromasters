4f29ccb82d1b30da4c18261db144a0b2ac357d3b
ailobe <aitorlopezbeltran@gmail.com>
2019-01-26 19:43:34 +0100

    Update fibonacci 1 and 2

--- algorithmic-design-and-techniques/algorithmic-warmup/fibonacci.cpp
+++ algorithmic-design-and-techniques/algorithmic-warmup/fibonacci.cpp
@@ -1,33 +1,46 @@
-#include <iostream>
 #include <cassert>
+#include <iostream>
 
 // The following code calls a naive algorithm for computing a Fibonacci number.
 //
 // What to do:
-// 1. Compile the following code and run it on an input "40" to check that it is slow.
-//    You may also want to submit it to the grader to ensure that it gets the "time limit exceeded" message.
+// 1. Compile the following code and run it on an input "40" to check that it is
+// slow.
+//    You may also want to submit it to the grader to ensure that it gets the
+//    "time limit exceeded" message.
 // 2. Implement the fibonacci_fast procedure.
-// 3. Remove the line that prints the result of the naive algorithm, comment the lines reading the input,//    uncomment the line with a call to test_solution, compile the program, and run it.
-//    This will ensure that your efficient algorithm returns the same as the naive one for small values of n.
-// 4. If test_solution() reveals a bug in your implementation, debug it, fix it, and repeat step 3.
-// 5. Remove the call to test_solution, uncomment the line with a call to fibonacci_fast (and the lines reading the input),
+// 3. Remove the line that prints the result of the naive algorithm, comment the
+// lines reading the input,//    uncomment the line with a call to
+// test_solution, compile the program, and run it.
+//    This will ensure that your efficient algorithm returns the same as the
+//    naive one for small values of n.
+// 4. If test_solution() reveals a bug in your implementation, debug it, fix it,
+// and repeat step 3.
+// 5. Remove the call to test_solution, uncomment the line with a call to
+// fibonacci_fast (and the lines reading the input),
 //    and submit it to the grader.
 
 int fibonacci_naive(int n) {
-    if (n <= 1)
-        return n;
+    if (n <= 1) return n;
 
     return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
 }
 
-int fibonacci_fast(int n) {
-    int arr[2+n] {0, 1};
-        
-    for (int i = 2; i <= n; ++i){
-        arr[i] = arr[i-1] + arr[i-2];
-    }       
+// Computes up to F(92) = 7'540'113'804'746'346'429, then overflows
+long long fibonacci_fast(int n) {
+    if (n <= 1) return n;
 
-    return arr[n];
+    long long c = 0;  // F(0)
+    long long b = 1;  // F(1)
+    long long a = 1;  // F(2)
+
+    for (int i = 3; i <= n; ++i) {
+        c = b;      // F(i-2) = F(i-1)
+        b = a;      // F(i-1) = F(i)
+        a = b + c;  // F(i) = F(-1) + F(-2)
+    }
+
+    return a;
 }
 
 void test_solution() {
@@ -38,11 +51,8 @@ void test_solution() {
 }
 
 int main() {
-    int n = 0; 
+    int n = 0;
     std::cin >> n;
-        
-    //std::cout << fibonacci_naive(n) << '\n';
-    //test_solution();
     std::cout << fibonacci_fast(n) << '\n';
     return 0;
 }
--- algorithmic-design-and-techniques/algorithmic-warmup/fibonacci_last_digit.cpp
+++ algorithmic-design-and-techniques/algorithmic-warmup/fibonacci_last_digit.cpp
@@ -3,11 +3,10 @@
 // Compute the Last Digit of a Large Fibonacci Number
 
 int get_fibonacci_last_digit_naive(int n) {
-    if (n <= 1)
-        return n;
+    if (n <= 1) return n;
 
     int previous = 0;
-    int current  = 1;
+    int current = 1;
 
     for (int i = 0; i < n - 1; ++i) {
         int tmp_previous = previous;
@@ -18,20 +17,26 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
-int get_fibonacci_last_digit_fast(int n) {
-    int arr[2+n] {0, 1};
-        
-    for (int i = 2; i <= n; ++i){
-        arr[i] = (arr[i-1] + arr[i-2]) % 10;
-    }       
+int get_fibonacci_last_digit_fast(long long n) {
+    if (n <= 1) return n;
 
-    return arr[n];
+    int c = 0;  // F(0)
+    int b = 1;  // F(1)
+    int a = 1;  // F(2)
+
+    for (int i = 3; i <= n; ++i) {
+        c = b;             // F(i-2) = F(i-1)
+        b = a;             // F(i-1) = F(i)
+        a = (b + c) % 10;  // F(i) = (F(-1) + F(-2)) mod 10
+    }
+
+    return a;
 }
 
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_fast(n);
-    std::cout << c << '\n';
-    }
+    std::cout << get_fibonacci_last_digit_fast(n) << '\n';
+    return 0;
+}
 
